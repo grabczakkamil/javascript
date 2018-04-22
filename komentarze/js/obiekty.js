22f904793efe0987e9aea2e78c3855e1bfb63894
Kamil Grabczak <grabczakkamil@gmail.com>
2018-04-22 12:57:49 +0200

    obiekty koniec

--- komentarze/js/obiekty.js
+++ komentarze/js/obiekty.js
@@ -1,3 +1,44 @@
 'use strict';
-var d = 333;
-console.log(d);
+
+class Osoba {
+    constructor(imie, nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+    wyswietlInfo() {
+        console.log("Nazywam się: " + this.imie + " " + this.nazwisko);
+    }
+}
+
+var agata = new Osoba("Agata", "Biesaga");
+var marcin = new Osoba("Marcin", "Kwiatkowski");
+
+console.log(agata.imie);
+
+console.log(agata);
+
+console.log(marcin);
+
+agata.wyswietlInfo();
+
+//var kaja = {
+//    imie: "Kaja",
+//    wzrost: 152,
+//    przedstawOsobe: function() {
+//        console.log(this.imie);
+//    }
+//}
+//
+//var krystian = {
+//    imie: "Krystian",
+//    wzrost: 180,
+//    przedstawOsobe: function() {
+//        console.log(this.imie);
+//    }
+//}
+//
+//kaja.przedstawOsobe();
+//krystian.przedstawOsobe();
+//
+//console.log(kaja.wzrost);
+//console.log(kaja);
