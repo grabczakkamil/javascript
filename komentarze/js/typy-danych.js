168bc6721f41714fed797533fe622e0feb6831b8
Kamil Grabczak <grabczakkamil@gmail.com>
2018-04-22 12:01:37 +0200

    typy danych koniec

---
+++ komentarze/js/typy-danych.js
@@ -0,0 +1,30 @@
+'use strict';
+    var wyplata = 5000;
+    var premia = 2500;
+    var calkowitaWyplata;
+    calkowitaWyplata = wyplata + premia;
+
+    console.log(calkowitaWyplata);
+
+    var wyplataStr = "5000";
+    var premiaStr = "2500";
+    var calkowitaWyplataStr;
+
+    calkowitaWyplataStr = wyplataStr + premiaStr;
+
+    console.log(calkowitaWyplataStr);
+
+    var czyJestSmog;
+    czyJestSmog = false;
+
+    if (czyJestSmog) {
+        console.log("Jest SMOG");
+    } else {
+        console.log("Nie ma SMOGa");
+    }
+/*typy specjalne - undefined*/
+var niezdefiniowanaZmienna;
+var nullZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullZmienna);
