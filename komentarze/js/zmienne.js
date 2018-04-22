6bec718f4792d78e61be0494dd7a38ca45e4520c
Kamil Grabczak <grabczakkamil@gmail.com>
2018-04-22 10:24:37 +0200

    zmienne koniec

---
+++ komentarze/js/zmienne.js
@@ -0,0 +1,13 @@
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "Sergio";
+
+imie = imieDamskie;
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
