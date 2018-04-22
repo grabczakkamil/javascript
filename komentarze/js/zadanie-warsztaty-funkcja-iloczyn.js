076d3b6f34607ad5233380c48b5560b1f289d7bc
Kamil Grabczak <grabczakkamil@gmail.com>
2018-04-22 16:53:13 +0200

    warsztaty funkcja iloczynu

---
+++ komentarze/js/zadanie-warsztaty-funkcja-iloczyn.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function iloczyn(parametr1, parametr2, parametr3) {
+
+    var wynik = parametr1 * parametr2 * parametr3;
+
+    return wynik;
+}
+var wynikFunkcji = iloczyn(1, 4, 5);
+console.log(wynikFunkcji);
