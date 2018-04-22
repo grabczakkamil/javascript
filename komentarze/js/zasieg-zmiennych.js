7837020eecf19c2ad3565626c9acab5188a98aa4
Kamil Grabczak <grabczakkamil@gmail.com>
2018-04-22 11:43:56 +0200

    zasieg zmiennych koniec

---
+++ komentarze/js/zasieg-zmiennych.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function multiply(l1, l2, l3) {
+    var result = l1 * l2 * l3;
+    
+    return result;
+}
+
+var wynikMnozenia = multiply(3,4,5);
+
+console.log(wynikMnozenia);
+
+
+//var parametr4 = 10;
+//
+//function multiply(parametr1, parametr2, parametr3 ) {
+//    
+//}
