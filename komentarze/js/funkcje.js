d55c1fbe7ff1a3a9db80380bf7c0b787eaefdab8
Kamil Grabczak <grabczakkamil@gmail.com>
2018-04-22 11:27:27 +0200

    zasięg zmiennych

---
+++ komentarze/js/funkcje.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function dodajLiczby(pierwszaLiczba, drugaLiczba) {
+    
+    var wynik = pierwszaLiczba + drugaLiczba;
+    
+//    console.log(wynik);
+    return wynik;
+}
+
+var wynikZFunkcji = dodajLiczby(5, 7);
+
+console.log(wynikZFunkcji);
+
+//dodajLiczby(4, 7);
+
+//function aktualnyRok() {
+//    console.log("2018");
+//}
+//
+//var aktualnyMiesiac = function() {
+//    console.log("Kwiecień");
+//}
+//
+//console.log(aktualnyMiesiac);
+//
+//aktualnyRok();
+//aktualnyRok();
+//
+//aktualnyMiesiac();
+
